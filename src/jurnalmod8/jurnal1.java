906eeb58b2126816f6b44b5c78d072796239a501
fahmi_6706150024 <robinpurnama07@gmail.com>
2017-04-06 09:38:31 +0700

    Update jurnal1.java

--- src/jurnalmod8/jurnal1.java
+++ src/jurnalmod8/jurnal1.java
@@ -12,22 +12,36 @@ import java.util.Scanner;
  * @author Praktikan
  */
 public class jurnal1 {
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci : ");
-        int n = show.nextInt();
-        long fib[] = new long[n];
-         
-        fib[0] = 0;
-        fib[1] = 1;
-         
-        for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
-        }
-         
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
-        }
-    }
+     public static void main(String[]args){
+System.out.println("Eko Pramono (103510281)");
+System.out.println("");
+int l = 1;
+int tingkat = 10;
+int n = 12/2;
+for(int i=1;i<=n;i++){
+for(int k=n;k>i;k--){
+System.out.print(" ");
+}
+for(int j=1;j<=l;j++){
+System.out.print("+");
+}
+l+=2;
+ 
+System.out.println("");
+}
+l -= 4;
+ 
+for(int i=(n-1);i>=1;i--){
+for(int k=i;k<=(n-1);k++){
+System.out.print(" ");
+}
+for(int j=l;j>=1;j--){
+System.out.print("+");
+}
+l-=2;
+ 
+System.out.println("");
+}
+ 
+}
 }
